85ee0869472822ca907160776f23071d0f89e5cd
NursultanBeken <nur.beken.usa@gmail.com>
2023-05-06 15:47:27 -0400

    fix tflint installation

--- scripts/terraform_install.sh
+++ scripts/terraform_install.sh
@@ -4,17 +4,13 @@
 export TF_VERSION=1.3.7
 export CHECKOV_VERSION=2.2.125
 export TFLINT_VERSION=0.39.1
-export TFLINT_AWS_RULESET_VERSION=0.15.0
 
+echo "Installing Terraform"
 wget -O terraform.zip "https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip"
 unzip terraform.zip && rm terraform.zip && mv terraform /usr/bin/terraform && chmod +x /usr/bin/terraform
 echo "============================================================"
+echo "Installing Checkov"
 pip install checkov==${CHECKOV_VERSION}
 echo "============================================================"
-curl --location https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip --output tflint_linux_amd64.zip && \
-unzip tflint_linux_amd64.zip && \
-rm tflint_linux_amd64.zip
-echo "============================================================"
-# curl --location https://github.com/terraform-linters/tflint-ruleset-aws/releases/download/v${TFLINT_AWS_RULESET_VERSION}/tflint-ruleset-aws_linux_amd64.zip --output tflint-ruleset-aws.zip && \
-# unzip tflint-ruleset-aws.zip -d ~/.tflint.d/plugins && \
-# rm tflint-ruleset-aws.zip
+echo "Installing TFLint"
+curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
